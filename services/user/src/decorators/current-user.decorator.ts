39a877fab663c7f65f879e15dd9978c5c8f7046b
SergeyArenzon <arenzonsergey@gmail.com>
2025-08-03 21:29:23 +0300

    type decorator

--- services/user/src/decorators/current-user.decorator.ts
+++ services/user/src/decorators/current-user.decorator.ts
@@ -4,6 +4,6 @@ import { UserDto } from '@rapid-guide-io/shared';
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): UserDto => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    return request.user as UserDto;
   },
 );
