959c52e7757fe7c4fe411d645a27e3910e2f28e4
SergeyArenzon <arenzonsergey@gmail.com>
2025-08-03 17:57:32 +0300

    migrate to user zod shared dtos

--- services/user/src/user/user.module.ts
+++ services/user/src/user/user.module.ts
@@ -7,15 +7,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
-        imports: [
-          JwtModule.registerAsync(jwtConfig.asProvider()),
-          MikroOrmModule.forFeature([User]),
-        ],
-        controllers: [UserController],
-        providers: [UserService],
-        exports: [UserService],  
-      }
-)
-export class UserModule {
-    
-}
+  imports: [
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    MikroOrmModule.forFeature([User]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {}
