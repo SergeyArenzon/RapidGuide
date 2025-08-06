b0abc4448919ad46631ae2cff99af72c3f598f13
SergeyArenzon <arenzonsergey@gmail.com>
2025-08-06 20:21:35 +0300

    add validaResponse api

--- services/tour/src/category/category.service.ts
+++ services/tour/src/category/category.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityManager, EntityRepository } from '@mikro-orm/core';
 import { Category } from './entities/category.entity';
 import { SubCategory } from '../sub-category/entities/sub-category';
-import { ResponseCategoryDto } from './dto/response-category.dto';
 
 
 @Injectable()
@@ -16,43 +13,32 @@ export class CategoryService {
   ) {}
 
 
-  async findAll(): Promise<ResponseCategoryDto[]> {
+  async findAll() {
     // Find all categories
-    const categories = await this.categoryRepository.findAll();
+    // const categories = await this.categoryRepository.findAll();
     
-    // Fetch all subcategories in a single query
-    const allSubcategories = await this.em.find(SubCategory, {});
+    // // Fetch all subcategories in a single query
+    // const allSubcategories = await this.em.find(SubCategory, {});
     
-    // Group subcategories by category id
-    const subcategoriesByCategory = allSubcategories.reduce((acc, subcategory) => {
-      const categoryId = subcategory.category.id;
-      if (!acc[categoryId]) {
-        acc[categoryId] = [];
-      }
-      acc[categoryId].push(subcategory);
-      return acc;
-    }, {} as Record<string, SubCategory[]>);
+    // // Group subcategories by category id
+    // const subcategoriesByCategory = allSubcategories.reduce((acc, subcategory) => {
+    //   const categoryId = subcategory.category.id;
+    //   if (!acc[categoryId]) {
+    //     acc[categoryId] = [];
+    //   }
+    //   acc[categoryId].push(subcategory);
+    //   return acc;
+    // }, {} as Record<string, SubCategory[]>);
     
-    // Map categories with their subcategories
-    const result = categories.map(category => {
-      return {
-        ...category,
-        subcategories: subcategoriesByCategory[category.id] || []
-      } as ResponseCategoryDto;
-    });
+    // // Map categories with their subcategories
+    // const result = categories.map(category => {
+    //   return {
+    //     ...category,
+    //     subcategories: subcategoriesByCategory[category.id] || []
+    //   } as ResponseCategoryDto;
+    // });
     
-    return result;
+    return [];
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
-
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
 }
