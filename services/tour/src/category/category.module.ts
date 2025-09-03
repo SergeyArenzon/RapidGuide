d71803d32937e8fd5e3d4f4bf53d1bc8322991e2
SergeyArenzon <arenzonsergey@gmail.com>
2025-09-03 21:36:54 +0300

    fix eslint

--- services/tour/src/category/category.module.ts
+++ services/tour/src/category/category.module.ts
@@ -3,7 +3,6 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Category } from './entities/category.entity';
-import { SubCategory } from '../sub-category/entities/sub-category';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Category])],
