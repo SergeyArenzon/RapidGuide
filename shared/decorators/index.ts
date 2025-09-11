60f6805d113a4c99784178247501f675a6f122c1
SergeyArenzon <arenzonsergey@gmail.com>
2025-09-11 12:37:34 +0300

    add shared interceptor

--- shared/decorators/index.ts
+++ shared/decorators/index.ts
@@ -2,4 +2,5 @@ export * from './src/roles.decorator';
 export * from './src/scope.decorator';
 export * from './src/subject.decorator';
 export * from './src/allowed-roles.decorator';
+export * from './src/response-schema.decorator';
 
