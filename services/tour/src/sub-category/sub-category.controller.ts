91b4f96f8c56c26bc11ff185e508dc3a3029bc1d
SergeyArenzon <sergeyr1991@gmail.com>
2025-11-30 15:14:13 +0200

    restore tour database dir

--- services/tour/src/sub-category/sub-category.controller.ts
+++ services/tour/src/sub-category/sub-category.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SubCategoryService } from './sub-category.service';
 import { SubCategoryDto } from '@rapid-guide-io/dto';
-import { RolesGuard, ScopesGuard } from '@rapid-guide-io/guards';
+import { ScopesGuard } from '@rapid-guide-io/guards';
 import {
-  Role,
-  Roles,
   ScopePermission,
   Scopes,
 } from '@rapid-guide-io/decorators';
@@ -14,8 +12,7 @@ export class SubCategoryController {
   constructor(private readonly subCategoryService: SubCategoryService) {}
 
   @Get()
-  @UseGuards(RolesGuard, ScopesGuard)
-  @Roles(Role.CLIENT)
+  @UseGuards(ScopesGuard)
   @Scopes([ScopePermission.SUBCATEGORY_READ])
   findAll(): Promise<SubCategoryDto[]> {
     return this.subCategoryService
--- services/tour/src/sub-category/sub-category.module.ts
+++ services/tour/src/sub-category/sub-category.module.ts
@@ -3,13 +3,10 @@ import { SubCategoryService } from './sub-category.service';
 import { SubCategoryController } from './sub-category.controller';
 import { SubCategory } from './entities/sub-category';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConfig } from 'src/config';
 
 @Module({
   imports: [
       MikroOrmModule.forFeature([SubCategory]),
-      JwtModule.registerAsync(jwtConfig.asProvider())
     ],
   controllers: [SubCategoryController],
   providers: [SubCategoryService],
