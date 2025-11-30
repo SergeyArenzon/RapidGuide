30c99542239fc3fa368c056ba5fdf0a4417ca003
SergeyArenzon <sergeyr1991@gmail.com>
2025-11-30 15:27:57 +0200

    fix

--- services/tour/src/main.ts
+++ services/tour/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+  });
   app.enableCors({ credentials: true });
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
