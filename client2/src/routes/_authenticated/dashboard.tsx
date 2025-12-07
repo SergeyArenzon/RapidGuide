406dda4ab3657c55fe60455398a0361f878481ff
SergeyArenzon <arenzonsergey@gmail.com>
2025-12-07 22:09:04 +0200

    delete handleMe

--- client2/src/routes/_authenticated/dashboard.tsx
+++ client2/src/routes/_authenticated/dashboard.tsx
@@ -1,18 +1,11 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import { Sidebar } from '@/components/Sidebar'
-import { Button } from '@/components/ui/button'
-import Api from '@/lib/api'
 
 export const Route = createFileRoute('/_authenticated/dashboard')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const handleMe = async () => {
-    const api = new Api();
-    const meData = await api.profile.getMe();
-    console.log(meData);
-  }
   return (
     <div className="grid grid-cols-[1fr_200px] grid-rows-[50px_1fr] h-full w-full">
         <nav>DASHBOARD</nav>
